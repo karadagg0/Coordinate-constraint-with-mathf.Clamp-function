9d765528b25655fe69aca496f20cadcba3425e1b
karadagg0 <104911016+karadagg0@users.noreply.github.com>
2022-10-31 16:33:39 +0300

    Unity algorithm
    Coordinate constraint to move the object

---
+++ Mathf.Clamp/Mathf.Clamp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Program : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float leftTarget, rightTarget;
+
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        transform.Translate(h * Time.deltaTime * speed * Vector2.right);
+
+
+
+        Vector2 temp = transform.position;
+        temp.x = Mathf.Clamp(temp.x, leftTarget, rightTarget);
+        transform.position = temp;
+
+
+
+    }
+}
+
